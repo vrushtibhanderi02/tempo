412e9eaf2c606faf4bb5f6e24d60c79142cf3db2
vrushtibhanderi02 <varushtibhanderi3@gmail.com>
2024-04-09 11:18:06 +0530

    first

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+
+ // pre-loader
+$(document).ready(function(){
+  setTimeout(function(){
+    $('.pre-loader').fadeOut();
+  },3000);
+});
+
+// bottomtop 
+var btn = $('.top-bottom');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+// sticky header
+$(window).scroll(function(){
+    var sticky = $('.sticky'),
+        scroll = $(window).scrollTop();
+
+    if (scroll >= 100) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+});
+
+
+// accordian conectivity
+$(function() {
+	// (Optional) Active an item if it has the class "is-active"	
+	$(".accordion > .accordion-item.is-active").children(".accordion-panel").slideDown();
+	
+	$(".accordion > .accordion-item").click(function() {
+		// Cancel the siblings
+		$(this).siblings(".accordion-item").removeClass("is-active").children(".accordion-panel").slideUp();
+		// Toggle the item
+		$(this).toggleClass("is-active").children(".accordion-panel").slideToggle("ease-out");
+	});
+});
